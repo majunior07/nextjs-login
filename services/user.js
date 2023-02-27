84e9407583c33309bf203f72f7034fb3b1fcddb8
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-02-27 16:34:24 -0300

    Segundo commit dia 27/02/23

--- services/user.js
+++ services/user.js
@@ -3,8 +3,15 @@ let users = []
 export function cadastro(body) {
     const user = users.find(({ email }) => email === body.email)
     if(user) throw new Error('Usuário já cadastrado')
+
+    users.push(body)
+    return body
 }
 
 export function login(body) {
+    const user = users.find(({ email }) => email === body.email)
+    if(!user) throw new Error('Usuário não encontrado')
+    if(user.password !== body.password) throw new Error('Senha incorreta')
 
+    return user
 }
