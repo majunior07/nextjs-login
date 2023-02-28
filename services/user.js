c10eb8764a117d3ac08c94e44aec6783335fa14d
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-02-28 19:18:43 -0300

    Terceiro commit dia 28/02/2023

--- services/user.js
+++ services/user.js
@@ -2,10 +2,18 @@ import jwt from 'jsonwebtoken'
 
 let users = []
 
-const SECRET = process.env.JWR_SECRET
+const SECRET = process.env.JWT_SECRET
 
 function createToken(user) {
-    return jwt.sign({ email: user.email, name: user.name}, SECRET)
+    return jwt.sign({ email: user.email, name: user.name }, SECRET)
+}
+
+function readToken(token) {
+    try {
+        return jwt.verify(token, SECRET)
+    } catch (err) {
+        throw new Error('Token inválido!')
+    }
 }
 
 export function cadastro(body) {
@@ -13,7 +21,9 @@ export function cadastro(body) {
     if(user) throw new Error('Usuário já cadastrado')
 
     users.push(body)
-    return body
+
+    const token = createToken(body)
+    return token
 }
 
 export function login(body) {
@@ -21,6 +31,7 @@ export function login(body) {
     if(!user) throw new Error('Usuário não encontrado')
     if(user.password !== body.password) throw new Error('Senha incorreta')
 
-    return user
+    const token = createToken(user)
+    return token    
 }
 
