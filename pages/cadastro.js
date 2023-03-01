e1aa94d8416dec883a051db90b151b79a7d72b80
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-03-01 18:52:26 -0300

    Terceiro commit dia 01/03/2023

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -1,3 +1,4 @@
+import { useState} from 'react'
 import Link from 'next/link'
 import styles from '../styles/Login.module.css'
 
@@ -6,13 +7,48 @@ import Input from '../src/components/input/input'
 import Button from '../src/components/button/button'
 
 export default function CadastroPage() {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    })
+
+    const handleFormEdit = (event, name) => {
+        setFormData({
+            ...formData, 
+            [name]: event.target.value
+        })
+    }
+
+    const handleForm = async (event) => {
+        //event.preventDefault()
+        //console.log(formData)
+        try {
+            event.preventDefault()
+            const response = await fetch(`/api/user/cadastro`, {
+                method: 'POST',
+                body: JSON.stringify(formData) 
+            })
+
+            const json = await response.json()
+            //console.log(response.status)
+            //console.log(json)
+
+            if(response.status !== 201) throw new Error(json)
+
+        } catch (err) {
+
+        }
+    }
+
     return(
         <div className={styles.background}>
             <LoginCard tittle="Crie sua conta">
-                <form className={styles.form}>
-                    <Input type="text" placeholder="Seu nome" />
-                    <Input type="email" placeholder="Seu e-mail" />
-                    <Input type="password" placeholder="Sua senha" />
+                <form onSubmit={handleForm} className={styles.form}>
+                    <Input type="text" placeholder="Seu nome" required value={formData.name} onChange={(e) => {handleFormEdit(e, 'name')}} />
+                    <Input type="email" placeholder="Seu e-mail" required value={formData.email} onChange={(e) => {handleFormEdit(e, 'email')}} />
+                    <Input type="password" placeholder="Sua senha" required value={formData.password} onChange={(e) => {handleFormEdit(e, 'password')}} />
                     <Button>Cadastrar</Button>
                     <Link href="/login" >Já possui uma conta?</Link>
                 </form>
